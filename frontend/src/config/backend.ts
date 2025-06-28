c689e0535e8e099cf0a69f80b7c19d370938cb59
Muhammed Irshad <muhammedirshadismail0722@gmail.com>
2025-06-28 15:30:25 +0530

    payment check

--- frontend/src/config/backend.ts
+++ frontend/src/config/backend.ts
@@ -1,6 +1,6 @@
 import { get } from "http";
 
-export const API_URL = "http://localhost:8000/api/v1";
+export const API_URL = `${import.meta.env.VITE_CASHFREE_APP_ID_PROD}/api/v1`;
 export const API_ENDPOINT = {
   PURCHASE_NEW_USER: `${API_URL}/purchase/e-book-course`,
   VERIFY_USER: `${API_URL}/user/me `,
